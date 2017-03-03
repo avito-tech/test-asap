5d5e2c5dabec93cce82cc6ed90a45c6e9e938045
Ivan Strelkov <istrelkov@avito.ru>
2017-03-03 15:57:46 +0300

    Proxy - experiment 2

--- proxy/server.js
+++ proxy/server.js
@@ -6,31 +6,16 @@ var app = express();
 
 var proxy = httpProxy.createProxyServer({});
 
-// var server = http.createServer(function(request, response) {
-//     proxy.web(request, response, { target: 'http://www.avito.dev' });
-// });
-// server.listen(8888);
-
-app.use('/', expressHttpProxy('www.avito.dev', {
-    // decorateRequest: function(proxyReq, originalReq) {
-    //     if (originalReq.hostname === 'gmail.com') {
-    //         //
-    //     } else {
-    //         //
-    //     }
-    //     return proxyReq;
-    // }
-    // intercept: function(rsp, data, req, res, callback) {
-    //     callback(null, JSON.stringify('f'));
-    // }
-    filter: function(req, res) {
-        return req.hostname !== 'actiagent.ru';
-    }
-}));
-app.listen(8888);
-
-app.all('/', function(req, resp) {
-    resp.redirect('https://avito.ru');
+app.use(function(req, res, next) {
+    console.log(req.hostname);
+    next();
 });
+
+app.use(function(req) {
+    var mw = expressHttpProxy(req.hostname);
+
+    mw.apply(this, arguments);
+});
+app.listen(8888);
 
 console.log('started');
