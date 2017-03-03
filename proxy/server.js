52867a9355b4858db9c0c49803261284074aea04
Ilya Kremer <ikremer@avito.ru>
2017-03-03 15:53:52 +0300

    Filter proxy requests

--- proxy/server.js
+++ proxy/server.js
@@ -2,13 +2,35 @@ var express = require('express');
 var http = require('http');
 var expressHttpProxy = require('express-http-proxy');
 var httpProxy = require('http-proxy');
-// var app = express();
+var app = express();
 
 var proxy = httpProxy.createProxyServer({});
 
-var server = http.createServer(function(request, response) {
-    proxy.web(request, response, { target: 'http://www.avito.dev' });
+// var server = http.createServer(function(request, response) {
+//     proxy.web(request, response, { target: 'http://www.avito.dev' });
+// });
+// server.listen(8888);
+
+app.use('/', expressHttpProxy('www.avito.dev', {
+    // decorateRequest: function(proxyReq, originalReq) {
+    //     if (originalReq.hostname === 'gmail.com') {
+    //         //
+    //     } else {
+    //         //
+    //     }
+    //     return proxyReq;
+    // }
+    // intercept: function(rsp, data, req, res, callback) {
+    //     callback(null, JSON.stringify('f'));
+    // }
+    filter: function(req, res) {
+        return req.hostname !== 'actiagent.ru';
+    }
+}));
+app.listen(8888);
+
+app.all('/', function(req, resp) {
+    resp.redirect('https://avito.ru');
 });
-server.listen(8888);
 
 console.log('started');
