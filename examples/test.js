5928aa8ade3a9d0a1af3a72d1a688e3ca0c323f9
Ilya Kremer <ikremer@avito.ru>
2017-03-04 16:54:59 +0300

    extension/tab.js: add isVisible method

--- examples/test.js
+++ examples/test.js
@@ -11,6 +11,7 @@ tat.start().then(Tab => {
     return Tab.create('https://avito.ru/moskva', {
         button: '.search.button',
         item: '.item',
+        error: '.banner-apps-error',
         lkLink: 'a'
     }).then((tab) => {
         return tab.typeText('#search', 'test text')
@@ -18,6 +19,9 @@ tat.start().then(Tab => {
             .then(() => tab.item.waitFor())
             .then(() => tab.item.getStyle('background-color'))
             .then((color) => assert(color == 'rgba(0, 0, 0, 0)', color))
+            .then(() => tab.error.waitFor())
+            .then(() => tab.error.isVisible())
+            .then(isVisible => assert(!isVisible, 'has errors'))
             .then(() => tab.item.countItems('.item'))
             .then((count) => assert(count >= 3, 'less than 3  - ' + count))
             .then(() => tab.getAttr('.item:nth-child(3) .title a', 'href'))
