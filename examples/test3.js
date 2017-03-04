2af7e01cec004e893622ee6ed1c35b227e5d49a0
Ivan Strelkov <istrelkov@avito.ru>
2017-03-04 11:05:03 +0300

    Added ability to reset stubs

--- examples/test3.js
+++ examples/test3.js
@@ -3,19 +3,29 @@ var tat = require('../');
 var sinon = require('sinon');
 var match = sinon.match;
 
-tat.stub.http.withArgs(
-    match.has('headers',
-        match.has('host',
-            match('avito.ru')
-        )
-    )
+tat.stub.https.withArgs(
+    match.any
 ).returns(
-    tat.respondWith.html('<h2>AVITO</h2>')
+    tat.respondWith.html('<h2>LOL</h2>')
 );
 
 tat.start().then(Tab => {
-    return Tab.create('http://avito.ru');
+    return Tab.create('https://avito.ru').then((tab) => {
+        return tab.waitFor('h2')
+            .then(() => tab.getText('h2'))
+            .then(text => console.log(text))
+            .then(() => tat.stub.reset())
+            .then(() => tab.reload())
+            .then(() => tab.waitFor('button'))
+            .then(() => tab.getText('button'))
+            .then(text => console.log(text))
+            .then(() => tab.close())
+            .catch((err) => {
+                console.log(err);
+                tab.close();
+            });
+    });
 }, err => {
     console.log(err);
-});
-// .then(() => tat.stop());
+})
+.then(() => tat.stop());
