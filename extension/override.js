5688763654f9613dd5afa014e1e32cf2f69aa6da
Ivan Strelkov <istrelkov@avito.ru>
2017-03-10 19:43:07 +0300

    Extension - try to add waitFor method (found click actually does not work)

--- extension/override.js
+++ extension/override.js
@@ -1,5 +1,3 @@
-console.log(io);
-
 var attach = function(debuggee) {
     return new Promise(function(resolve) {
         chrome.debugger.attach(debuggee, '1.2', resolve);
@@ -54,15 +52,20 @@ function getKeyCode(char) {
     }
 }
 
+const WAITING_TIMEOUT_IN_MILLISECS = 10000;
+
 class Tab {
     constructor(debuggee) {
         this.debuggee = debuggee;
+        this._documentUpdatedHandlers = [];
     }
 
     click(querySelector) {
         querySelector = querySelector.replace('"', '\\"');
 
-        return this._command('Runtime.evaluate', { expression: 'document.querySelector("' + querySelector + '").scrollIntoViewIfNeeded()' })
+        return this._command('Runtime.evaluate', {
+            expression: 'document.querySelector("' + querySelector + '").scrollIntoViewIfNeeded()'
+        })
             .then(() =>
                 this._command('Runtime.evaluate', {
                     expression: 'rect = document.querySelector("' + querySelector + '").getBoundingClientRect(), { top: rect.top, bottom: rect.bottom, left: rect.left, right: rect.right }',
@@ -153,6 +156,86 @@ class Tab {
 
     close() {
         return new Promise(resolve => chrome.tabs.remove(this.debuggee.tabId, resolve));
+    }
+
+    countItems(selector) {
+        return new Promise((resolve) => {
+            this._command('Runtime.evaluate', {
+                expression: 'document.querySelectorAll("' + selector + '").length',
+                returnByValue: true
+            })
+                .then(result => {
+                    resolve(result.result.value);
+                });
+        });
+    }
+
+    _onDocumentUpdated() {
+        alert('asdfadsf');
+        let args = arguments;
+
+        this._documentUpdatedHandlers.forEach(handler => handler.apply(this, args));
+    }
+
+    _handleDocumentUpdated(fn) {
+        if (this._documentUpdatedHandled) {
+            return Promise.resolve();
+        }
+        chrome.debugger.onEvent.addListener(this._onDocumentUpdated);
+
+        this._documentUpdatedHandlers.push(fn);
+    }
+
+    _unhandleDocumentUpdated(fn) {
+        this._documentUpdatedHandlers =
+            this._documentUpdatedHandlers.filter(h => h != fn);
+    }
+
+    _handleForAppearance(selector, timeout) {
+        return new Promise((resolve, reject) => {
+            let timeoutId;
+
+            let handler = () => {
+                this.countItems(selector)
+                    .then(exists => {
+                        if (exists) {
+                            this._unhandleDocumentUpdated(handler);
+                            clearTimeout(timeoutId);
+                            resolve();
+                        }
+                    });
+            };
+
+            this._handleDocumentUpdated(handler);
+
+            timeoutId = setTimeout(() => {
+                this._unhandleDocumentUpdated(handler);
+                reject();
+            }, timeout);
+        });
+    }
+
+    waitFor(selector) {
+        return this._enableDOM()
+            .then(() => this.countItems(selector))
+            .then(present => {
+                if (present > 0) {
+                    return Promise.resolve();
+                } else {
+                    return this._handleForAppearance(selector, WAITING_TIMEOUT_IN_MILLISECS);
+                }
+            });
+    }
+
+    _enableDOM() {
+        if (this._domEnabled) {
+            return Promise.resolve();
+        } else {
+            return this._command('DOM.enable')
+                .then(() => {
+                    this._domEnabled = true;
+                });
+        }
     }
 }
 
