7bf5d2a42a23bb9ff0cf341b1f88d916a08a100e
Ivan Strelkov <istrelkov@avito.ru>
2017-03-24 21:54:01 +0300

    Added respondWith.jsonInterceptor

--- test/integration/test.js
+++ test/integration/test.js
@@ -1,59 +1,123 @@
 /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 'use strict';
 
-const tat = require('../..');
+const testAsap = require('../..');
 const sinon = require('sinon');
 const match = sinon.match;
 const fs = require('fs');
+const http = require('http');
 
 const htmlText = fs.readFileSync(__dirname + '/test.html', 'utf8');
 
-tat.stub.https.withArgs(
-    match.has('url', match('index.html'))
-).returns(function(ctx) {
-    const res = ctx.proxyToClientResponse;
-
-    res.setHeader('Content-Type', 'text/html');
-    res.end(htmlText);
-});
-
-tat.stub.https.withArgs(
-    match.has('url', match('1.json?q=some'))
-).returns(function(ctx) {
-    const res = ctx.proxyToClientResponse;
-
-    res.setHeader('Content-Type', 'application/json');
-    res.end('{"tag": "span", "inner": "secret"}');
-});
-
-function assert(condition, message) {
-    return new Promise((resolve, reject) => {
-        return condition ? resolve(condition) : reject({ message });
+describe('Test ASAP', function(){
+    beforeAll(function(done) {
+        testAsap.start().then(Tab => {
+            this.Tab = Tab;
+            done();
+        });
     });
-}
 
-let exitCode = 0;
-
-tat.start().then(Tab => {
-    return Tab.create('https://avito.ru/index.html').then((tab) => {
-        return tab.typeText('input', 'some')
-            .then(() => tab.waitForVisible('button'))
-            .then(() => tab.click('button'))
-            .then(() => tab.waitFor('span'))
-            .then(() => tab.getText('span'))
-            .then(text => assert(text == 'secret', 'expected "secret" but got ' + text))
-            .then(() => tab.close())
-            .then(() => console.log('✅   OK!'))
-            .catch((err) => {
-                exitCode = 1;
-                console.log('🚫   Error: ');
-                console.log(err);
-                tab.close();
-            });
+    afterAll(function(done) {
+        testAsap.stop().then(done);
     });
-}, err => {
-    exitCode = 2;
-    console.log(err);
-})
-.then(() => tat.stop())
-.then(() => process.exit(exitCode));
+
+    beforeEach(function() {
+        testAsap.stub.reset();
+    });
+
+    afterEach(function(done) {
+        if (this.tab) {
+            this.tab.close().then(done, done.fail);
+        } else {
+            done();
+        }
+    });
+
+    it('respondWith.jsonInterceptor', function(done) {
+        let jsonServer;
+        const response = {
+            val: 'abc',
+            items: [
+                { name: 'a' },
+                { name: 'b' }
+            ]
+        };
+        const expected = JSON.stringify(modify(clone(response)));
+
+        function clone(obj) {
+            return JSON.parse(JSON.stringify(obj));
+        }
+
+        function modify(response) {
+            response.val = response.val + 'def';
+            response.items.length = 1;
+
+            return response;
+        }
+
+        function startServer() {
+            return new Promise(resolve => {
+                jsonServer = http.createServer((req, res) => {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.writeHead(200);
+                    res.end(JSON.stringify(response));
+                });
+
+                jsonServer.listen(8080, resolve);
+            })
+        }
+        function stopServer() {
+            return new Promise(resolve => jsonServer.close(resolve));
+        }
+
+        function setInterceptor() {
+            testAsap.stub.http.withArgs(
+                testAsap.match.url('secret.json')
+            ).returns(
+                testAsap.respondWith.jsonInterceptor(modify)
+            )
+        }
+
+        testAsap.stub.http.withArgs(
+            testAsap.match.url('respondWith.html')
+        ).returns(
+            testAsap.respondWith.file(__dirname + '/respondWith.html')
+        );
+
+        startServer()
+            .then(setInterceptor)
+            .then(() => this.Tab.create('http://127.0.0.1:8080/respondWith.html'))
+            .then(tab => this.tab = tab)
+            .then(() => this.tab.waitFor('#result'))
+            .then(() => this.tab.getText('#result'))
+            .then(text => {
+                expect(text).toBe(expected);
+            })
+            .then(stopServer)
+            .then(done, done.fail)
+    });
+
+    it('just works', function(done) {
+        testAsap.stub.https.withArgs(
+            match.has('url', match('index.html'))
+        ).returns(
+            testAsap.respondWith.file(__dirname + '/test.html')
+        );
+
+        testAsap.stub.https.withArgs(
+            match.has('url', match('1.json?q=some'))
+        ).returns(
+            testAsap.respondWith.json({tag: 'span', inner: 'secret'})
+        );
+
+        this.Tab.create('https://avito.ru/index.html')
+            .then(tab => { this.tab = tab; })
+            .then(() => this.tab.typeText('input', 'some'))
+            .then(() => this.tab.waitForVisible('button'))
+            .then(() => this.tab.click('button'))
+            .then(() => this.tab.waitFor('span'))
+            .then(() => this.tab.getText('span'))
+            .then(text => expect(text).toBe('secret'))
+            .then(done, done.fail);
+    });
+});
